f333628180a51a8f01fbd12df3bce8f11f5d24cb
Hilario Gonzalez <114438952+hilariog@users.noreply.github.com>
2024-09-18 00:04:45 -0400

    Update exercise_light.py

--- assignment/exercise_light.py
+++ assignment/exercise_light.py
@@ -9,7 +9,7 @@ import machine
 # GP28 is ADC2
 ADC2 = 28
 
-led = machine.Pin(25, machine.Pin.OUT)
+led = machine.Pin(17, machine.Pin.OUT)
 adc = machine.ADC(ADC2) #adc28
 
 blink_period = 0.1
@@ -39,9 +39,9 @@ while True:
     duty_cycle = clip((value - min_bright) / (max_bright - min_bright))# want min bright to equal value at low, and max bright to equal value at high
 
 
-    #led.high()
+    led.high()
     time.sleep(blink_period * duty_cycle)#we want basically full bkink period sleep when high and zero sleep when low i.e, duty_cycle = 1 and 0
 
-    #led.low()
+    led.low()
     time.sleep(blink_period * (1 - duty_cycle))
 
